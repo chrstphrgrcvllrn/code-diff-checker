08e26e9b2346d10e85a5c53148d64bc3298a0881
christopher-villarin <christopher.villarin@hogarth.com>
2026-08-26 21:32:37 +0800

    Add Buy me a coffee 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 // 
-  // base: "/code-diff-checker/",
+  base: "/code-diff-checker/",
 
   server: {
     port: 5173
